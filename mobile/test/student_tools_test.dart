33970872cf7844a17cc00b044423fa528328484e
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-08-14 18:28:01 +0545

    test: add mobile student tool coverage

---
+++ mobile/test/student_tools_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/services/student_tools.dart';
+
+void main() {
+  test('calculates weighted GPA', () {
+    expect(StudentTools.gpa([4, 3], [3, 3]), closeTo(3.5, 0.001));
+  });
+
+  test('calculates attendance percentage', () {
+    expect(StudentTools.attendance(18, 20), closeTo(90, 0.001));
+  });
+
+  test('finds classes needed for target attendance', () {
+    expect(StudentTools.classesNeededForTarget(attended: 15, total: 20, target: 80), 5);
+  });
+}
